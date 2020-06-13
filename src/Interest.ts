212e7710cd8cb37f35f1fbe6b6e9c12f08d2ee29
TheDudeFromCI <thedudefromci@gmail.com>
2020-06-12 17:36:05 -0700

    Added passive interest on spawned entities.

--- src/Interest.ts
+++ src/Interest.ts
@@ -1,4 +1,6 @@
 import { Vec3 } from "vec3";
+import { Entity } from "prismarine-entity";
+import { Bot } from "mineflayer";
 
 /**
  * A data object representing the mental stimuli that the bot feels toward an object.
@@ -56,7 +58,22 @@ export interface InterestHolder
  */
 export class InterestDatabase
 {
-    private interestHolders: Array<InterestHolder> = new Array<InterestHolder>();
+    private readonly interestHolders: Array<InterestHolder> = new Array<InterestHolder>();
+    private readonly entities: Array<EntityInterestHolder> = new Array();
+    readonly bot: Bot;
+
+    /**
+     * Creates a new interest database.
+     * 
+     * @param bot - The bot this interest database is for.
+     */
+    constructor(bot: Bot)
+    {
+        this.bot = bot;
+
+        this.bot.on("entitySpawn", (e) => this.onEntitySpawned(e));
+        this.bot.on("entityGone", (e) => this.onEntityDespawned(e));
+    }
 
     /**
      * Adds a new interest holder to this database, to be considered.
@@ -79,4 +96,115 @@ export class InterestDatabase
         if (index > -1)
             this.interestHolders.splice(index, 1);
     }
+
+    /**
+     * Gets the most interesting object in this list.
+     * 
+     * @returns The most interesting object, or null if this list is empty.
+     */
+    getMostInteresting(): InterestHolder | null
+    {
+        if (this.interestHolders.length == 0)
+            return null;
+
+        let best = this.interestHolders[0];
+        let value = best.getInterest().getValue();
+
+        for (let i = 1; i < this.interestHolders.length; i++)
+        {
+            let newValue = this.interestHolders[i].getInterest().getValue();
+            if (newValue > value)
+            {
+                best = this.interestHolders[i];
+                value = newValue;
+            }
+        }
+
+        return best;
+    }
+
+    /**
+     * Called when a new entity is added to the world.
+     * 
+     * @param entity - The entity which was spawned.
+     */
+    private onEntitySpawned(entity: Entity): void
+    {
+        if (!entity || entity === this.bot.entity)
+            return;
+
+        console.log(`Registered new entity '${entity.displayName}' to interest database.`);
+
+        const holder = new EntityInterestHolder(this.bot, entity);
+        this.entities.push(holder);
+
+        this.addInterestHolder(holder);
+    }
+
+    /**
+     * Called when a new entity is remove from the world.
+     *
+     * @param entity - The entity which was spawned.
+     */
+    private onEntityDespawned(entity: Entity): void
+    {
+        if (!entity || entity === this.bot.entity)
+            return;
+
+        console.log(`Unregistered entity '${entity.displayName}' from interest database.`);
+
+        let holder = this.getEntityInterestHolder(entity);
+
+        if (holder)
+            this.removeInterestHolder(holder);
+    }
+
+    /**
+     * Gets the interest holder for the given entity.
+     * 
+     * @param entity - The entity.
+     */
+    private getEntityInterestHolder(entity: Entity): EntityInterestHolder | null
+    {
+        for (let i = 0; i < this.entities.length; i++)
+            if (this.entities[i].entity === entity)
+                return this.entities[i];
+
+        return null;
+    }
+}
+
+/**
+ * A wrapper for entities which can hold the bot's interest.
+ */
+class EntityInterestHolder implements InterestHolder
+{
+    private readonly interest: Interest = new Interest();
+    readonly bot: Bot;
+    readonly entity: Entity;
+
+    constructor(bot: Bot, entity: Entity)
+    {
+        this.bot = bot;
+        this.entity = entity;
+    }
+
+    getLookPosition(): typeof Vec3
+    {
+        // @ts-ignore
+        return this.entity.position.offset(0, this.entity.height, 0);
+    }
+
+    getInterest(): Interest
+    {
+        // @ts-ignore
+        let distance = this.entity.position.distanceTo(this.bot.entity.position);
+        distance = 5 / distance; // scale interest inversely with distance.
+
+        // TODO Set interest to 0 if bot can't see entity.
+
+        this.interest.setValue(distance);
+        return this.interest;
+    }
+
 }
